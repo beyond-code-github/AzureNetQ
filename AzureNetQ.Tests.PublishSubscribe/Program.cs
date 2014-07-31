24fafc6d27ec995c7d5687567ee4f50f164d96bf
Pete Smith <roysvork@gmail.com>
2014-07-31 18:40:05 +0100

    Added non-generic subscribe extensions and test project

---
+++ AzureNetQ.Tests.PublishSubscribe/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace AzureNetQ.Tests.PublishSubscribe
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using AzureNetQ.NonGeneric;
+
+    public class Program
+    {
+        private IBus bus;
+
+        public static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            this.bus = AzureBusFactory.CreateBus("Endpoint=sb://servicebus/ServiceBusDefaultNamespace;StsEndpoint=https://servicebus:10355/ServiceBusDefaultNamespace;RuntimePort=10354;ManagementPort=10355");
+
+            this.bus.SubscribeAsync(
+                typeof(TestMessage),
+                obj => Task.Factory.StartNew(() => Console.WriteLine("Handler Received message: {0}", ((TestMessage)obj).Text)));
+                
+            Console.WriteLine("Type a message or 'q' to quit.");
+
+            string text = null;
+            while ((text = Console.ReadLine()) != "q")
+            {
+                this.bus.Publish(
+                        typeof(TestMessage),
+                        new TestMessage
+                        {
+                            Text = text
+                        });
+            }
+
+            this.bus.Dispose();
+        }
+    }
+
+    [Serializable]
+    public class TestMessage
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ AzureNetQ/NonGeneric/NonGenericExtensions.cs
@@ -0,0 +1,72 @@
+﻿namespace AzureNetQ.NonGeneric
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using AzureNetQ.FluentConfiguration;
+
+    public static class NonGenericExtensions
+    {
+        public static IDisposable Subscribe(this IBus bus, Type messageType, Action<object> onMessage)
+        {
+            return Subscribe(bus, messageType, onMessage, configuration => { });
+        }
+
+        public static IDisposable Subscribe(
+            this IBus bus,
+            Type messageType,
+            Action<object> onMessage,
+            Action<ISubscriptionConfiguration> configure)
+        {
+            Preconditions.CheckNotNull(onMessage, "onMessage");
+
+            Func<object, Task> asyncOnMessage = x => TaskHelpers.ExecuteSynchronously(() => onMessage(x));
+
+            return SubscribeAsync(bus, messageType, asyncOnMessage, configure);
+        }
+
+        public static IDisposable SubscribeAsync(
+            this IBus bus,
+            Type messageType,
+            Func<object, Task> onMessage)
+        {
+            return SubscribeAsync(bus, messageType, onMessage, configuration => { });
+        }
+
+        public static IDisposable SubscribeAsync(
+            this IBus bus,
+            Type messageType,
+            Func<object, Task> onMessage,
+            Action<ISubscriptionConfiguration> configure)
+        {
+            Preconditions.CheckNotNull(bus, "bus");
+            Preconditions.CheckNotNull(messageType, "messageType");
+            Preconditions.CheckNotNull(onMessage, "onMessage");
+            Preconditions.CheckNotNull(configure, "configure");
+
+            var subscribeMethodOpen = typeof(IBus)
+                .GetMethods()
+                .SingleOrDefault(x => x.Name == "SubscribeAsync" && HasCorrectParameters(x));
+
+            if (subscribeMethodOpen == null)
+            {
+                throw new AzureNetQException("API change? SubscribeAsync method not found on IBus");
+            }
+
+            var subscribeMethod = subscribeMethodOpen.MakeGenericMethod(messageType);
+            return (IDisposable)subscribeMethod.Invoke(bus, new object[] { onMessage, configure });
+        }
+        
+        private static bool HasCorrectParameters(MethodInfo methodInfo)
+        {
+            var parameters = methodInfo.GetParameters();
+
+            return
+                parameters.Length == 2 &&
+                parameters[0].ParameterType.Name == "Func`2" &&
+                parameters[1].ParameterType == typeof(Action<ISubscriptionConfiguration>);
+        }
+    }
+}
